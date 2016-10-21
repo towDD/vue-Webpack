63837bd78ff4aa0a65586aca8b3081ae6ccaaa47
towDD <aaabbb00163@163.com>
2016-10-21 14:06:14 +0800

    Create app.vue

---
+++ src/app.vue
@@ -0,0 +1,33 @@
+<template>
+	<div class="main">
+        <router-view
+          keep-alive
+          transition="fade"
+          transition-mode='out-in'></router-view>
+  </div>
+</template>
+<script>
+import store from './vuex/store.js';
+
+(function () {
+    document.addEventListener('DOMContentLoaded', function () {
+        var html = document.documentElement;
+        var windowWidth = html.clientWidth;
+        html.style.fontSize = windowWidth / 7.2 + 'px';
+        // 等价于html.style.fontSize = windowWidth / 640 * 100 + 'px';
+    }, false);
+})();
+
+export default {
+  store:store
+}
+
+
+require("./st/css/app.scss");
+</script>
+
+
+<style lang="sass">
+
+
+</style>
